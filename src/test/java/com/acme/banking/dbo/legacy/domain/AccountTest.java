96b5bca8d432446d7dd6f041952d2bec30394210
izh <orcvadim@gmail.com>
2019-04-10 10:52:55 +0300

    test

--- src/test/java/com/acme/banking/dbo/legacy/domain/AccountTest.java
+++ src/test/java/com/acme/banking/dbo/legacy/domain/AccountTest.java
@@ -14,6 +14,7 @@ public class AccountTest {
         accountStructure.amount = 0;
 
         //Look ma, no checks!
+        //test
         accountStructure.amount -= 2_000;
 
         assertThat(accountStructure.amount)
